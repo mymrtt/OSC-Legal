cebef39a33eb47f59c8cb4a0db3c339097e648ed
mymrtt <mymrtt@gmail.com>
2020-03-02 16:24:12 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <p>Oscc Legal</p>
-    </div>
-  );
+	return (
+		<div>
+			<p>Oscc Legal</p>
+		</div>
+	);
 }
 
 export default App;
