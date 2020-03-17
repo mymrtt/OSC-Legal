ac7f0802cc6221831a33eda127b8ee6f0ddfc54b
mymrtt <mymrtt@gmail.com>
2020-03-17 18:16:49 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 // Libs
 import React from 'react';
 
-<<<<<<< HEAD
-function App() {
-  return (
-    <div>
-      <p>Osc Legal</p>
-    </div>
-  );
-}
-=======
 // Routes
 import Routes from './routes/routes';
 
 const App = () => <Routes />;
->>>>>>> ac0029e38cf2de3625eb57df2532ee127d0e5d92
 
 export default App;
