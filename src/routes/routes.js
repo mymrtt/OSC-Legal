5118852ac3c33cfa7c76926f1fa2a84e4b503410
mymrtt <mymrtt@gmail.com>
2020-03-20 17:18:23 -0300

    teste

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -18,12 +18,12 @@ import CreateUserScreen from '../screens/onboarding/User/CreateUserScreen';
 import Dashboard from '../screens/dashboard/index';
 
 import PrivateRoute from './PrivateRoute';
-import NoMatch from '../NoMatch';
 
 const Routes = () => (
 	<BrowserRouter>
 		<Switch>
 			<Route exact path='/' component={Login} />
+			<Route component={Login} />
 			<Route path='/login' component={Login} />
 			<Route path='/loginreset' component={LoginResetPasswordScreen} />
 			<Route path='/resetcode' component={ResetPasswordCode} />
@@ -31,8 +31,6 @@ const Routes = () => (
 			<Route path='/newpassword' component={NewPassowrdScreen} />
 			<Route path='/createuser' component={CreateUserScreen} />
 			{/* <Route path='/organization' component={Organization} /> */}
-
-			<Route component={NoMatch} />
 			<PrivateRoute component={Dashboard} />
 		</Switch>
 	</BrowserRouter>
