b14f06dab3f1fb8748cf7df5aa13ac656943844e
mymrtt <mymrtt@gmail.com>
2020-03-20 17:15:44 -0300

    Updates

---
+++ precache-manifest.f932d2550edd3c33a9b684e9e934a7eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd2b174270fe95084c1cfa938dcc6c3",
+    "url": "/OSC-Legal/index.html"
+  },
+  {
+    "revision": "37d57b51805f35b8d17a",
+    "url": "/OSC-Legal/static/css/main.c0180cdc.chunk.css"
+  },
+  {
+    "revision": "34f165bd7825ab441b01",
+    "url": "/OSC-Legal/static/js/2.8a47af85.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/OSC-Legal/static/js/2.8a47af85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d57b51805f35b8d17a",
+    "url": "/OSC-Legal/static/js/main.4b4dc35f.chunk.js"
+  },
+  {
+    "revision": "566b1579a03fae3d130a",
+    "url": "/OSC-Legal/static/js/runtime-main.6c64655e.js"
+  },
+  {
+    "revision": "e049bfe2356ee3f9dde758622c704ef6",
+    "url": "/OSC-Legal/static/media/logo.e049bfe2.svg"
+  },
+  {
+    "revision": "c5fe52db3ca0c72085f218f42049eb80",
+    "url": "/OSC-Legal/static/media/sucess.c5fe52db.svg"
+  },
+  {
+    "revision": "f95eae64c150b0aef03432210bbc41d5",
+    "url": "/OSC-Legal/static/media/visibility-off.f95eae64.svg"
+  },
+  {
+    "revision": "54d5a3d6ccea73de0100839e4239d2df",
+    "url": "/OSC-Legal/static/media/visibility-on.54d5a3d6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.baca15327eba45f58c6d44517b8f9fc1.js"
+  "/OSC-Legal/precache-manifest.f932d2550edd3c33a9b684e9e934a7eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OSC-Legal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
