a646d404f500508eb20116e3064d5a30d5ce92f8
Erlane Mendes <erlane.mendes31@gmail.com>
2020-04-06 17:29:36 -0300

    changing names to english

--- src/screens/onboarding/User/CreateUserScreen.js
+++ src/screens/onboarding/User/CreateUserScreen.js
@@ -383,28 +383,28 @@ class CreateUserScreen extends Component {
 
  isValidCPF = () => {
 	 const { cpf } = this.state.user;
-	 let numeros; let digitos; let soma; let i; let resultado;
-	 let	digitos_iguais;
- 	digitos_iguais = 1;
+	 let numbers; let digits; let sum; let i; let result;
+	 let digitsEqual;
+ 	digitsEqual = 1;
  	if (cpf.length < 11) return false;
  	for (i = 0; i < cpf.length - 1; i++) {
  		if (cpf.charAt(i) != cpf.charAt(i + 1)) {
- 			digitos_iguais = 0;
+ 			digitsEqual = 0;
  			break;
  		}
  	}
- 	if (!digitos_iguais) {
- 		numeros = cpf.substring(0, 9);
- 		digitos = cpf.substring(9);
- 		soma = 0;
- 		for (i = 10; i > 1; i--) soma += numeros.charAt(10 - i) * i;
- 		resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
- 		if (resultado != digitos.charAt(0)) return false;
- 		numeros = cpf.substring(0, 10);
- 		soma = 0;
- 		for (i = 11; i > 1; i--) soma += numeros.charAt(11 - i) * i;
- 		resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
- 		if (resultado != digitos.charAt(1)) return false;
+ 	if (!digitsEqual) {
+ 		numbers = cpf.substring(0, 9);
+ 		digits = cpf.substring(9);
+ 		sum = 0;
+ 		for (i = 10; i > 1; i--) sum += numbers.charAt(10 - i) * i;
+ 		result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+ 		if (result != digits.charAt(0)) return false;
+ 		numbers = cpf.substring(0, 10);
+ 		sum = 0;
+ 		for (i = 11; i > 1; i--) sum += numbers.charAt(11 - i) * i;
+ 		result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+ 		if (result != digits.charAt(1)) return false;
  		return true;
  	}
  	return false;
