d50f3b92924ade75efe404f96ed022ff2972cb94
mymrtt <mymrtt@gmail.com>
2020-04-08 11:25:44 -0300

    fixing reusable components and adding editor config

--- src/components/Input.js
+++ src/components/Input.js
@@ -21,17 +21,17 @@ export const InputForm = styled.input`
   }
 `;
 
-export default function Input(props) {
-	return (
-		<InputForm
-			login={props.login}
-			type={props.type}
-			onChange={props.onChange}
-			value={props.value}
-			placeholder={props.placeholder}
-			name={props.name}
-			isError={props.isError}
-			required={props.required}
-		/>
-	);
-}
+const Input = props => (
+	<InputForm
+		login={props.login}
+		type={props.type}
+		onChange={props.onChange}
+		value={props.value}
+		placeholder={props.placeholder}
+		name={props.name}
+		isError={props.isError}
+		required={props.required}
+	/>
+);
+
+export default Input;
--- src/screens/onboarding/Login/LoginResetPasswordScreen.js
+++ src/screens/onboarding/Login/LoginResetPasswordScreen.js
@@ -1,203 +1,203 @@
-// Libs
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+// // Libs
+// import React, { Component } from 'react';
+// import styled from 'styled-components';
+// import { Redirect } from 'react-router-dom';
+// import { connect } from 'react-redux';
 
-// Components
-import ImageLogo from '../../../components/ImageLogo';
-import Input from '../../../components/Input';
-import Button from '../../../components/Button';
+// // Components
+// import ImageLogo from '../../../components/ImageLogo';
+// import Input from '../../../components/Input';
+// import Button from '../../../components/Button';
 
-// Images
-import VisibilityOn from '../../../assets/visibility-on.svg';
-import VisibilityOff from '../../../assets/visibility-off.svg';
+// // Images
+// import VisibilityOn from '../../../assets/visibility-on.svg';
+// import VisibilityOff from '../../../assets/visibility-off.svg';
 
-// Redux
-const mapStateToProps = state => ({
-	onboarding: state.onboarding,
-});
+// // Redux
+// const mapStateToProps = state => ({
+// 	onboarding: state.onboarding,
+// });
 
-const ContainerForm = styled.div`
-  height: 100vh;
-  background-color: #FFCFCD;
-  display: flex;
-  align-items: center;
-	justify-content: center;
-  margin: 0;
+// const ContainerForm = styled.div`
+//   height: 100vh;
+//   background-color: #FFCFCD;
+//   display: flex;
+//   align-items: center;
+// 	justify-content: center;
+//   margin: 0;
 
-  @media (max-width: 648px) {
-		background-color: #fff;
-	}
-`;
+//   @media (max-width: 648px) {
+// 		background-color: #fff;
+// 	}
+// `;
 
-const Form = styled.form`
-  width: 32%;
-  background-color: #fff;
-	border-radius: 5px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-	padding-top: 3rem;
+// const Form = styled.form`
+//   width: 32%;
+//   background-color: #fff;
+// 	border-radius: 5px;
+//   display: flex;
+//   align-items: center;
+//   flex-direction: column;
+// 	padding-top: 3rem;
 
-	@media (max-width: 980px) {
-		width: 70%;
-	}
+// 	@media (max-width: 980px) {
+// 		width: 70%;
+// 	}
 
-  @media (max-width: 648px) {
-		width: 100%;
-	}
-`;
+//   @media (max-width: 648px) {
+// 		width: 100%;
+// 	}
+// `;
 
-const Title = styled.p`
-	width: 70%;
-	color: #231F20;
-	font-size: 1rem;
-	font-family: Overpass,Regular;
-	margin-bottom: 2rem;
-	display: flex;
-	justify-content: center;
-`;
+// const Title = styled.p`
+// 	width: 70%;
+// 	color: #231F20;
+// 	font-size: 1rem;
+// 	font-family: Overpass,Regular;
+// 	margin-bottom: 2rem;
+// 	display: flex;
+// 	justify-content: center;
+// `;
 
-const InputBox = styled.span`
-	width: 75%;
-	display: flex;
-	flex-direction: column;
-	position: relative;
-`;
+// const InputBox = styled.span`
+// 	width: 75%;
+// 	display: flex;
+// 	flex-direction: column;
+// 	position: relative;
+// `;
 
-const ImagePassword = styled.img`
-  position: absolute;
-  bottom: ${props => (props.off ? '1.2rem' : '0.875rem')};
-  right: 0.7rem;
-  cursor: pointer;
-`;
+// const ImagePassword = styled.img`
+//   position: absolute;
+//   bottom: ${props => (props.off ? '1.2rem' : '0.875rem')};
+//   right: 0.7rem;
+//   cursor: pointer;
+// `;
 
-const ErrorMessage = styled.h4`
-  width: 75%;
-  color: #D53B40;
-  display: flex;
-  justify-content: flex-end;
-  font-size: 0.6rem;
-  font-family: Eurostile, Medium;
+// const ErrorMessage = styled.h4`
+//   width: 75%;
+//   color: #D53B40;
+//   display: flex;
+//   justify-content: flex-end;
+//   font-size: 0.6rem;
+//   font-family: Eurostile, Medium;
 
-  @media (max-width: 648px) {
-		width: 85%;
-	}	
-`;
+//   @media (max-width: 648px) {
+// 		width: 85%;
+// 	}
+// `;
 
-const Label = styled.label`
-  color: #85144b;
-  font-size: 0.75rem;
-  font-family: Overpass;
-	font-weight: bold;
-  margin-top: 1rem;
-  margin-bottom: 0.3rem;
-	padding-left: 0.8rem;
-	text-transform: uppercase;
-`;
+// const Label = styled.label`
+//   color: #85144b;
+//   font-size: 0.75rem;
+//   font-family: Overpass;
+// 	font-weight: bold;
+//   margin-top: 1rem;
+//   margin-bottom: 0.3rem;
+// 	padding-left: 0.8rem;
+// 	text-transform: uppercase;
+// `;
 
-class LoginResetPasswordScreen extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: '',
-			email: '',
-			password: '',
-			passwordError: '',
-			error: undefined,
-			type: 'password',
-			redirect: undefined,
-		};
-	}
+// class LoginResetPasswordScreen extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			value: '',
+// 			email: '',
+// 			password: '',
+// 			passwordError: '',
+// 			error: undefined,
+// 			type: 'password',
+// 			redirect: undefined,
+// 		};
+// 	}
 
-	handleSubmit = (ev) => {
-		ev.preventDefault();
+// 	handleSubmit = (ev) => {
+// 		ev.preventDefault();
 
-		if (this.state.password.length < 6) {
-			this.setState({
-				error: true,
-			});
-		} else {
-			this.setState({
-				error: false,
-			});
-		}
-	}
+// 		if (this.state.password.length < 6) {
+// 			this.setState({
+// 				error: true,
+// 			});
+// 		} else {
+// 			this.setState({
+// 				error: false,
+// 			});
+// 		}
+// 	}
 
-	handleChangeEmail = (ev) => {
-		this.setState({
-			email: ev.target.value,
-		});
-	};
+// 	handleChangeEmail = (ev) => {
+// 		this.setState({
+// 			email: ev.target.value,
+// 		});
+// 	};
 
-	handleChangePassword = (ev) => {
-		this.setState({
-			password: ev.target.value,
-		});
-	}
+// 	handleChangePassword = (ev) => {
+// 		this.setState({
+// 			password: ev.target.value,
+// 		});
+// 	}
 
-	handleChangeType = () => {
-		this.setState({
-			type: this.state.type === 'password' ? 'text' : 'password',
-		});
-	}
+// 	handleChangeType = () => {
+// 		this.setState({
+// 			type: this.state.type === 'password' ? 'text' : 'password',
+// 		});
+// 	}
 
-	render() {
-		const {
-			email, error, type, redirect,
-		} = this.state;
-		return (
-			<ContainerForm>
-				<Form onSubmit={this.handleSubmit}>
-					<ImageLogo />
-					<InputBox>
-						<Title>A senha de {}
-							({this.props.onboarding.emailReset ? this.props.onboarding.emailReset : 'nome@email.com'}) {}
-							foi redefinida, faça login para acessar o seu painel.</Title>
-						<Label>e-mail</Label>
-						<Input
-							login
-							type="email"
-							onChange={this.handleChangeEmail}
-							placeholder="nome@email.com"
-							value={email}
-							required
-						/>
-					</InputBox>
-					<InputBox>
-						<Label>senha</Label>
-						<Input
-							login
-							type={type}
-							isError={error}
-							required
-							placeholder="Insira senha"
-							onChange={this.handleChangePassword}
-						/>
-						<span>
-							<ImagePassword
-								src={type === 'password' ? VisibilityOn : VisibilityOff}
-								off={type === 'password'}
-								onClick={this.handleChangeType}
-							/>
-						</span>
-					</InputBox>
-					{error && <ErrorMessage>E-mail e/ ou senha incorreta</ErrorMessage>}
-					<Button
-						width='75%'
-						height='3.5rem'
-						heightMobile='3.5rem'
-						margin='1.5rem 0 5rem 0'
-						marginMobile='3.5rem 0 1rem'
-						text="entrar"
-						type="submit"
-					/>
-				</Form>
-				{redirect && <Redirect to={'/loginreset'} />}
-			</ContainerForm>
-		);
-	}
-}
+// 	render() {
+// 		const {
+// 			email, error, type, redirect,
+// 		} = this.state;
+// 		return (
+// 			<ContainerForm>
+// 				<Form onSubmit={this.handleSubmit}>
+// 					<ImageLogo />
+// 					<InputBox>
+// 						<Title>A senha de {}
+// 							({this.props.onboarding.emailReset ? this.props.onboarding.emailReset : 'nome@email.com'}) {}
+// 							foi redefinida, faça login para acessar o seu painel.</Title>
+// 						<Label>e-mail</Label>
+// 						<Input
+// 							login
+// 							type="email"
+// 							onChange={this.handleChangeEmail}
+// 							placeholder="nome@email.com"
+// 							value={email}
+// 							required
+// 						/>
+// 					</InputBox>
+// 					<InputBox>
+// 						<Label>senha</Label>
+// 						<Input
+// 							login
+// 							type={type}
+// 							isError={error}
+// 							required
+// 							placeholder="Insira senha"
+// 							onChange={this.handleChangePassword}
+// 						/>
+// 						<span>
+// 							<ImagePassword
+// 								src={type === 'password' ? VisibilityOn : VisibilityOff}
+// 								off={type === 'password'}
+// 								onClick={this.handleChangeType}
+// 							/>
+// 						</span>
+// 					</InputBox>
+// 					{error && <ErrorMessage>E-mail e/ ou senha incorreta</ErrorMessage>}
+// 					<Button
+// 						width='75%'
+// 						height='3.5rem'
+// 						heightMobile='3.5rem'
+// 						margin='1.5rem 0 5rem 0'
+// 						marginMobile='3.5rem 0 1rem'
+// 						text="entrar"
+// 						type="submit"
+// 					/>
+// 				</Form>
+// 				{redirect && <Redirect to={'/loginreset'} />}
+// 			</ContainerForm>
+// 		);
+// 	}
+// }
 
-export default connect(mapStateToProps)(LoginResetPasswordScreen);
+// export default connect(mapStateToProps)(LoginResetPasswordScreen);
