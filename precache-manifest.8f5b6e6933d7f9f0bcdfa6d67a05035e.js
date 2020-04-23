456ed635a2990138e159401808fa7aedd8cd945d
mymrtt <mymrtt@gmail.com>
2020-04-23 18:44:54 -0300

    Updates

---
+++ precache-manifest.8f5b6e6933d7f9f0bcdfa6d67a05035e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6805dda223f9df54494c2cc5136dca16",
+    "url": "/OSC-Legal/index.html"
+  },
+  {
+    "revision": "2ae43711406cdeb68674",
+    "url": "/OSC-Legal/static/css/main.c0180cdc.chunk.css"
+  },
+  {
+    "revision": "baa6aadce4d7f24c5ecd",
+    "url": "/OSC-Legal/static/js/2.0cf4593d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/OSC-Legal/static/js/2.0cf4593d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae43711406cdeb68674",
+    "url": "/OSC-Legal/static/js/main.667ad9ba.chunk.js"
+  },
+  {
+    "revision": "566b1579a03fae3d130a",
+    "url": "/OSC-Legal/static/js/runtime-main.6c64655e.js"
+  },
+  {
+    "revision": "3ce4b5b42e8ea57a1487682de052ecd6",
+    "url": "/OSC-Legal/static/media/authorization.3ce4b5b4.svg"
+  },
+  {
+    "revision": "fa1297e779925977cbb9a55c1c0d51c2",
+    "url": "/OSC-Legal/static/media/caminho.fa1297e7.svg"
+  },
+  {
+    "revision": "33d53cbeab9a8f4f169b82028312b343",
+    "url": "/OSC-Legal/static/media/delete.33d53cbe.svg"
+  },
+  {
+    "revision": "f522e40b6ed209a70cf1c723f8a25043",
+    "url": "/OSC-Legal/static/media/edit.f522e40b.svg"
+  },
+  {
+    "revision": "99e97a45baa377d860702b46f8447f33",
+    "url": "/OSC-Legal/static/media/extendDeadline.99e97a45.svg"
+  },
+  {
+    "revision": "303833dcbb7190875a9ab6a76326c380",
+    "url": "/OSC-Legal/static/media/fechar.303833dc.svg"
+  },
+  {
+    "revision": "5defe7c6e8a964c0baab99ba51b5bff0",
+    "url": "/OSC-Legal/static/media/free.5defe7c6.svg"
+  },
+  {
+    "revision": "e049bfe2356ee3f9dde758622c704ef6",
+    "url": "/OSC-Legal/static/media/logo.e049bfe2.svg"
+  },
+  {
+    "revision": "92a1b9acfc969829207c06d1ecf2f60b",
+    "url": "/OSC-Legal/static/media/pay.92a1b9ac.svg"
+  },
+  {
+    "revision": "7e764e9f6b59d27bafc76eaa35905eaf",
+    "url": "/OSC-Legal/static/media/setaLado.7e764e9f.svg"
+  },
+  {
+    "revision": "e237ad431b8ba94fae91da3075a6299b",
+    "url": "/OSC-Legal/static/media/sucess.e237ad43.svg"
+  },
+  {
+    "revision": "f95eae64c150b0aef03432210bbc41d5",
+    "url": "/OSC-Legal/static/media/visibility-off.f95eae64.svg"
+  },
+  {
+    "revision": "54d5a3d6ccea73de0100839e4239d2df",
+    "url": "/OSC-Legal/static/media/visibility-on.54d5a3d6.svg"
+  }
+]);
