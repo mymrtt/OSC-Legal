6bb0f9465b101fe749b8c010cb15604b057cba2b
Erlane Mendes <erlane.mendes31@gmail.com>
2020-05-12 17:39:01 -0300

    clean code

--- src/screens/onboarding/User/CreateUserScreen.js
+++ src/screens/onboarding/User/CreateUserScreen.js
@@ -419,14 +419,12 @@ class CreateUserScreen extends Component {
 			this.setState({
 				isErrorTel: true,
 			});
-			console.log('certo');
 		} else {
 			this.setState({
 				isErrorTel: false,
 			});
 			this.handleErrors();
 		}
-		console.log(this.state.isErrorTel);
 	};
 
 	handleErrors = () => {
