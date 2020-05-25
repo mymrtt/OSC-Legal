80dfb37f6893ac7d830066eebcfe321a68eac93c
mymrtt <mymrtt@gmail.com>
2020-05-25 11:12:57 -0300

    adding ex to api

---
+++ src/api.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+export const API_URL = 'http://localhost:3333';
+
+// Onboarding
+export const createAccount = (token, user) => axios({
+	url: `${API_URL}/users`,
+	method: 'post',
+	headers: {
+		Authorization: `Bearer ${token}`,
+	},
+	data: user,
+});
+
+export const login = user => axios({
+	url: `${API_URL}/auth/authenticate`,
+	method: 'post',
+	headers: {
+		Authorization: `Basic ${user}`,
+	},
+	data: user,
+});
+// Onboarding
+
+// Organization
+export const createOrganization = org => axios({
+	url: `${API_URL}/organizations`,
+	method: 'post',
+	data: org,
+});
+// Organization
+
+// Document
+export const createDocument = doc => axios({
+	url: `${API_URL}/documents`,
+	method: 'post',
+	data: doc,
+});
+// Document
+
+// Template (Document - Admin)
+export const createTemplate = template => axios({
+	url: `${API_URL}/templates`,
+	method: 'post',
+	data: template,
+});
+// Template (Document - Admin)
